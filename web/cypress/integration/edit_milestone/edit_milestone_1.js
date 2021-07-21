263e329f2f6cdbc6b5f28312ffa0d7e936cda6fd
ngoc_diem <ngocdiem5102000@gmail.com>
2021-07-21 15:46:35 +0700

    update convert halfwidth

---
+++ web/cypress/integration/edit_milestone/edit_milestone_1.js
@@ -0,0 +1,31 @@
+import Color from 'color'
+describe('Edit Milestone Test', () => {
+  const expectTextColor = Color('#2d334a').string() 
+    it('Visits Jobfair Support Edit Milestone', () => {
+      cy.visit('http://jobfair.local:8000/milestones/3/edit')
+    })
+    it('Check title', () => {
+      cy.get('.title').contains('マイルストーン編集').should('have.css', 'color')
+      .and('eq', expectTextColor)
+    })
+    it('Check label', () => {
+      
+      cy.get('form label p').contains('マイルストーン名').should('have.css', 'color')
+      .and('eq', expectTextColor)
+      cy.get('form label').should('have.attr', 'class', 'ant-form-item-required')
+      cy.get('form label p').contains('期日').should('have.css', 'color')
+      .and('eq', expectTextColor)
+    })
+    it('Check input name milestone', () => {
+      cy.get('input[id=basic_name]').type('text').invoke('val').then((val) => {
+        if(val === ''){
+          cy.get(div).should('have.attr','role','alert').contains('この項目は必須です。')
+        }
+        if(val.includes(' ')){
+          cy.get(div).should('have.attr','role','alert').contains('マイルストーン名はスペースが含まれていません。')
+
+        }
+      })
+      
+    })
+  })
