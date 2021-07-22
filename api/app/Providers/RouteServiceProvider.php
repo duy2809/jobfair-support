ec3a949f43792d183ea0e014b863a588d053d007
pham van tho <pham.van.tho@sun-asterisk.com>
2021-07-22 09:27:19 +0700

    feat: listmember3

--- api/app/Providers/RouteServiceProvider.php
+++ api/app/Providers/RouteServiceProvider.php
@@ -77,7 +77,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-         Route::prefix('api')
+        Route::prefix('api')
             ->middleware(['web'])
             ->namespace($this->namespace)
             ->group(base_path('routes/api.php'));
