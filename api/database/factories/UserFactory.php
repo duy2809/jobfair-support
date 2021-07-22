9a473e4b1f8a3f676aa97fe62a65938c35ba4ba5
anhqh-0149 <quach.hoang.anh@sun-asterisk.com>
2021-07-22 10:59:20 +0700

    fix sun ci

--- api/database/factories/UserFactory.php
+++ api/database/factories/UserFactory.php
@@ -5,7 +5,6 @@ namespace Database\Factories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
-use Faker\Generator as Faker;
 
 class UserFactory extends Factory
 {
@@ -23,13 +22,14 @@ class UserFactory extends Factory
      */
     public function definition()
     {
-        $role = $this->faker->randomElement([0,1]);
-        $records = mt_rand(0,10);
+        $role = $this->faker->randomElement([0, 1]);
+        $records = mt_rand(0, 10);
+
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'avatar' => $this->faker->imageUrl($width=200, $height=200, 'dogs'),
+            'avatar' => $this->faker->imageUrl($width = 200, $height = 200, 'dogs'),
             'role' => $role,
             'number_of_record' => $records,
             'chatwork_id' => Str::random(10),
