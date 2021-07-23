52253732b10e91622a0f210c8a75ec6fe6b811b6
ngoc_diem <ngocdiem5102000@gmail.com>
2021-07-23 15:44:28 +0700

    api add milestone

--- web/src/pages/milestones/add/index.jsx
+++ web/src/pages/milestones/add/index.jsx
@@ -10,20 +10,19 @@ import {
   Col,
 } from 'antd'
 import OtherLayout from '../../../layouts/OtherLayout'
+import { addMilestone } from '../../../api/milestone'
+
 
 export default function AddMilestonePage() {
   const [form] = Form.useForm()
 
   const [isModalVisible, setIsModalVisible] = useState(false)
   const [isModalVisibleOfCancel, setIsModalVisibleOfCancel] = useState(false)
+  const [typePeriodInput, setTypePeriodInput] = useState(0)
+  const [nameInput, setNameInput] = useState('')
+  const [timeInput, setTimeInput] = useState('')
 
   const { Option } = Select
-
-  const onValueNameChange = (e) => {
-    form.setFieldsValue({
-      name: toHalfWidth(e.target.value),
-    })
-  }
 
   function toHalfWidth(fullWidthStr) {
     return fullWidthStr.replace(/[Ａ-Ｚａ-ｚ０-９]/g, (s) => String.fromCharCode(s.charCodeAt(0) - 0xfee0))
@@ -46,7 +45,28 @@ export default function AddMilestonePage() {
   const handleOk = () => {
     form.submit()
     setIsModalVisible(false)
-    openNotificationSuccess()
+    console.log(timeInput)
+    addMilestone({
+      name: nameInput,
+      period: timeInput,
+      is_week: typePeriodInput,
+      schedule_id: 1,
+    }).then(() => openNotificationSuccess())
+      .catch((error) => {
+        if (JSON.parse(error.response.request.response).errors.name[0] === 'The name has already been taken.') {
+          notification.error({
+            message: 'このマイルストーン名は存在しています',
+          })
+        }
+        console.log(error.response)
+      })
+  }
+
+  const onValueNameChange = (e) => {
+    setNameInput(e.target.value)
+  }
+  const onValueTimeChange = (e) => {
+    setTimeInput(e.target.value)
   }
 
   const handleCancel = () => {
@@ -65,6 +85,10 @@ export default function AddMilestonePage() {
     <Form.Item name="typePeriod" noStyle>
       <Select
         className="select-after"
+        onChange={(value) => {
+        setTypePeriodInput(parseInt(value, 10))
+      }}
+      value={typePeriodInput.toString()}
         style={{
           width: 90,
         }}
@@ -159,6 +183,7 @@ export default function AddMilestonePage() {
                         /* style={{ width: '100%' }} */
                         addonAfter={selectAfter}
                         onKeyDown={blockInvalidChar}
+                        onChange={onValueTimeChange}
                         min={0}
                       />
                     </Form.Item>
