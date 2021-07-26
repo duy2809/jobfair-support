84f0637ba2f469d95cec6a21c9147d03f2937f72
pham van tho <pham.van.tho@sun-asterisk.com>
2021-07-26 23:22:09 +0700

    Hello

---
+++ web/src/pages/member/[id]/edit/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../../../../layouts/OtherLayout'
+import './styles.scss'
+
+export default function EditMember() {
+  return (
+    <Layout>
+      <Layout.Main>
+        <div className="text-6xl">Hello</div>
+      </Layout.Main>
+    </Layout>
+  )
+}
