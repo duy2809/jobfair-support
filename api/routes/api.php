e459fe5e6c17073743ac7941f4a3d34ea50efa8d
quynhbh-0153 <bui.huong.quynh@sun-asterisk.com>
2021-07-28 21:04:42 +0700

    check CI

--- api/routes/api.php
+++ api/routes/api.php
@@ -22,4 +22,3 @@ Route::group(['prefix' => 'jobfair/{id}'], function () {
     Route::get('/updated-tasks', 'JobfairController@updatedTasks');
     Route::get('/tasks/search', 'JobfairController@searchTask');
 });
-
