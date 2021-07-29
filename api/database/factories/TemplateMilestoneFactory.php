4b78aa2981d84305f7b954f1bd491d138f13dca3
Phong Vu UET <vuphonghyuet@gmail.com>
2021-07-29 11:47:58 +0700

    edit api for template milestones

---
+++ api/database/factories/TemplateMilestoneFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\TemplateMilestone;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TemplateMilestoneFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = TemplateMilestone::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => preg_replace('/\s/', '_', $this->faker->unique()->name()),
+            'period' => $this->faker->numberBetween(1, 50),
+            'is_week' => $this->faker->numberBetween(0,1),
+        ];
+    }
+}
---
+++ api/database/migrations/2021_07_29_040219_add_is_week_to_template_milestones_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIsWeekToTemplateMilestonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('template_milestones', function (Blueprint $table) {
+            $table->boolean('is_week');
+            $table->renameColumn('milestone_name', 'name');
+            $table->unsignedInteger('period')->change(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('template_milestones', function (Blueprint $table) {
+            $table->renameColumn('name', 'milestone_name');
+            $table->dropColumn('is_week');
+            $table->dropColumn('period');
+        });
+    }
+}
