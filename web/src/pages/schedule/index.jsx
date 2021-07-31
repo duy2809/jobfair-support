0a85b20a5993aa99910418541be1f82e040a029b
pham van tho <pham.van.tho@sun-asterisk.com>
2021-07-31 08:58:39 +0700

    fix :merge design

--- web/src/pages/schedule/index.jsx
+++ web/src/pages/schedule/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react'
-import { Select, Input, Table } from 'antd'
-import { SearchOutlined, PlusCircleOutlined } from '@ant-design/icons'
+import { Select, Input, Table, Empty, Button } from 'antd'
+import { SearchOutlined } from '@ant-design/icons'
 import { useRouter } from 'next/router'
 import Layout from '../../layouts/OtherLayout'
 import './styles.scss'
@@ -29,18 +29,7 @@ export default function ScheduleList() {
   const [filterSchedules, setFilterSchedules] = useState([])
   const [itemCount, setItemCount] = useState(10)
   const [dataLoading, setDataLoading] = useState(false)
-  const [pagination, setPagination] = useState({ position: ['bottomCenter'], current: 1, pageSize: 10 })
-
-  const fetchData = useCallback(() => {
-    setDataLoading(true)
-    ListScheduleApi.getListShedule().then((res) => {
-      const { data } = res
-      setSchedules(data)
-      setFilterSchedules(data)
-    }).finally(() => {
-      setDataLoading(false)
-    })
-  })
+  const [pagination, setPagination] = useState({ position: ['bottomCenter'], current: 1, pageSize: 10, showSizeChanger: false })
 
   const handleSelect = (value) => {
     setPagination((preState) => ({
@@ -48,6 +37,7 @@ export default function ScheduleList() {
       pageSize: value,
     }))
     setItemCount(value)
+    localStorage.setItem('pagination', JSON.stringify({ ...pagination, pageSize: value }))
   }
 
   const handleChange = (e) => {
@@ -61,6 +51,27 @@ export default function ScheduleList() {
     const result = schedules.filter((obj) => obj.name.toLowerCase().indexOf(e.target.value.toLowerCase()) > -1)
     setFilterSchedules(result)
   }
+
+  const initPagination = () => {
+    const paginationData = JSON.parse(localStorage.getItem('pagination'))
+    if (paginationData === null) {
+      localStorage.setItem('pagination', JSON.stringify(pagination))
+    } else {
+      setItemCount(paginationData.pageSize)
+    }
+  }
+
+  const fetchData = useCallback(() => {
+    setDataLoading(true)
+    initPagination()
+    ListScheduleApi.getListShedule().then((res) => {
+      const { data } = res
+      setSchedules(data)
+      setFilterSchedules(data)
+    }).finally(() => {
+      setDataLoading(false)
+    })
+  })
 
   const router = useRouter()
   const handleClick = (e) => {
@@ -81,6 +92,7 @@ export default function ScheduleList() {
     <Layout>
       <Layout.Main>
         <div className="flex flex-col h-full items-center justify-center bg-white-background">
+          <div className="text-5xl w-10/12 font-bold py-10 title" style={{ fontSize: '36px' }}>JFスケジュール一覧</div>
           <div className="justify-start w-10/12">
             <span className="text-xl">表示件数: </span>
             <Select className="ml-5" defaultValue={10} onChange={handleSelect}>
@@ -91,13 +103,34 @@ export default function ScheduleList() {
           </div>
           <div className="flex justify-between w-10/12">
             <div className="text-2xl ml-auto flex items-center">
-              { role === 'super admin' ? (
-                <PlusCircleOutlined className="mx-8" onClick={handleClick} />
-              ) : ''}
-              <Input placeholder="探索" onChange={handleInput} bordered prefix={<SearchOutlined />} />
+              <Input placeholder="スケジュール" onChange={handleInput} bordered prefix={<SearchOutlined />} />
             </div>
           </div>
-          <Table className="w-10/12 rounded-3xl font-bold table-styled my-5 table-striped-rows" dataSource={filterSchedules} pagination={pagination} onRow={handleRow} columns={columns} isLoading={dataLoading} onChange={handleChange} />
+          <Table
+            className="w-10/12 rounded-3xl font-bold table-styled my-5 table-striped-rows"
+            columns={columns}
+            dataSource={filterSchedules}
+            rowKey={(record) => record.id}
+            scroll={{ y: 360 }}
+            onRow={handleRow}
+            onChange={handleChange}
+            loading={dataLoading}
+            pagination={pagination}
+            locale={{ emptyText: <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description="該当結果が見つかりませんでした" /> }}
+          />
+          <div className="flex w-9/12 justify-end">
+            { role === 'super admin' ? (
+              <Button
+                type="primary"
+                className="ml-5 px-12"
+                htmlType="button"
+                enabled
+                onClick={handleClick}
+              >
+                追加
+              </Button>
+            ) : ''}
+          </div>
         </div>
       </Layout.Main>
     </Layout>
