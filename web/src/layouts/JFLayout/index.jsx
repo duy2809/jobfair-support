387ae875c4a50f948624fe71dfa4c4fe78b547ad
Do Duy Anh <do.duy.anh@sun-asterisk.com>
2021-08-03 10:22:06 +0700

    change route sidebar

--- web/src/layouts/JFLayout/index.jsx
+++ web/src/layouts/JFLayout/index.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import { Layout, Menu } from 'antd'
-import Link from 'next/link'
-import PropTypes from 'prop-types'
-import _get from 'lodash/get'
+import React, { useEffect, useState } from 'react';
+import { Layout, Menu } from 'antd';
+import Link from 'next/link';
+import PropTypes from 'prop-types';
+import _get from 'lodash/get';
 import {
   HomeOutlined,
   FileProtectOutlined,
@@ -11,24 +11,22 @@ import {
   FileOutlined,
   ArrowRightOutlined,
   ArrowLeftOutlined,
-} from '@ant-design/icons'
-import { findSlot } from '../../utils/pages'
-import Navbar from '../../components/navbar'
-import './style.scss'
+} from '@ant-design/icons';
+import { findSlot } from '../../utils/pages';
+import Navbar from '../../components/navbar';
+import './style.scss';
 
-const JfLayout = ({
-  children,
-}) => {
-  const main = findSlot(JfLayout.Main, children)
-  const { Sider, Content } = Layout
-  const [collapsed, Setcollapsed] = useState(false)
+const JfLayout = ({ children }) => {
+  const main = findSlot(JfLayout.Main, children);
+  const { Sider, Content } = Layout;
+  const [collapsed, Setcollapsed] = useState(false);
   const toggleCollapsed = () => {
-    Setcollapsed(!collapsed)
-  }
-  const [idJF, setIdJf] = useState('')
+    Setcollapsed(!collapsed);
+  };
+  const [idJF, setIdJf] = useState('');
   useEffect(() => {
-    setIdJf(localStorage.getItem('id-jf'))
-  }, [children])
+    setIdJf(localStorage.getItem('id-jf'));
+  }, [children]);
   return (
     <div className="menuu">
       <Layout className="site-layout" style={{ marginLeft: 0 }}>
@@ -44,9 +42,7 @@ const JfLayout = ({
         >
           <Menu
             style={{
-
               height: '100vh',
-
             }}
             defaultSelectedKeys={['1']}
             defaultOpenKeys={['sub1']}
@@ -67,10 +63,10 @@ const JfLayout = ({
               </div>
             </div>
             <Menu.Item key="1" icon={<HomeOutlined />}>
-              <Link href={`/jf-toppage/${idJF}`}>ホーム</Link>
+              <Link href={`/jf-top/${idJF}`}>ホーム</Link>
             </Menu.Item>
             <Menu.Item key="2" icon={<FileProtectOutlined />}>
-              <Link href={`/task-list/${idJF}`}>タスク</Link>
+              <Link href={`/tasks/${idJF}`}>タスク</Link>
             </Menu.Item>
             <Menu.Item key="3" icon={<BarChartOutlined />}>
               <Link href={`/grantt-chart/${idJF}`}>ガントチャート</Link>
@@ -93,24 +89,23 @@ const JfLayout = ({
             }}
             // style={{ margin: '24px 16px 0', overflow: 'initial' }}
             className="site-layout-background"
-
           >
-            { _get(main, 'props.children') }
+            {_get(main, 'props.children')}
           </Content>
         </Layout>
       </Layout>
     </div>
-  )
-}
-JfLayout.Main = () => null
+  );
+};
+JfLayout.Main = () => null;
 
 JfLayout.defaultProps = {
   children: [],
-}
+};
 JfLayout.propTypes = {
   children: PropTypes.oneOfType([
     PropTypes.node,
     PropTypes.arrayOf(PropTypes.node),
   ]),
-}
-export default JfLayout
+};
+export default JfLayout;
