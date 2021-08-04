b2dd3f64cafc763a8d9227a6800f0fc605b8e63a
VuTan <tanboy042001@gmail.com>
2021-08-04 09:13:15 +0700

    fix CI

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         foreach ($schedules as $schedule) {
             \App\Models\Milestone::factory(10)->has(\App\Models\Task::factory()->count(3))->create(['schedule_id' => $schedule->id]);
         }
+
         \App\Models\User::factory(10)->create();
         \App\Models\TemplateMilestone::factory(50)->create();
     }
