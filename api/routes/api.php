e8fc961318b6cf57a89dc39a066215fde8ad5752
Phong Vu UET <vuphonghyuet@gmail.com>
2021-08-04 17:36:05 +0700

    restore api route file

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\JobfairController;
 use App\Http\Controllers\ResetPasswordController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,11 @@ use Illuminate\Support\Facades\Route;
  */
 
 Route::get('/web-init', WebInit::class);
+
+Route::prefix('member')->group(function () {
+    Route::get('/', 'MemberController@index');
+});
+
 Route::resource('/jobfair', 'JobfairController');
 Route::group(['prefix' => 'jobfair/{id}'], function () {
     Route::get('/milestones', 'JobfairController@getMilestones');
@@ -23,6 +29,23 @@ Route::group(['prefix' => 'jobfair/{id}'], function () {
     Route::get('/updated-tasks', 'JobfairController@updatedTasks');
     Route::get('/tasks/search', 'JobfairController@searchTask');
 });
+// add jf route start
+Route::post('/is-jf-existed', [JobfairController::class, 'checkNameExisted']);
+Route::resource('/jobfair', 'JobfairController');
+Route::resource('/schedules', 'ScheduleController');
+Route::get('/schedules/{id}/milestones', 'ScheduleController@getMilestones');
+Route::get('/schedules/{id}/tasks', 'ScheduleController@getTasks');
+Route::get('/admins', 'AdminController@index');
+
+Route::group(['prefix' => 'jobfair/{id}'], function () {
+    Route::get('/milestones', 'JobfairController@getMilestones');
+    Route::get('/tasks', 'JobfairController@getTasks');
+    Route::get('/updated-tasks', 'JobfairController@updatedTasks');
+    Route::get('/tasks/search', 'JobfairController@searchTask');
+    Route::get('/tasks/search', 'JobfairController@searchTask');
+    Route::post('/jobfair', 'JobfairController@store');
+});
+//add JF route end
 
 Route::resource('/milestone', TemplateMilestoneController::class);
 Route::get('/milestone/search', 'TemplateMilestoneController@getSearch');
@@ -36,3 +59,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout']);
 Route::post('/reset-password', [ResetPasswordController::class, 'handleRequest']);
 Route::post('/update-password', [ResetPasswordController::class, 'updatePassword']);
+
+Route::resource('/jf-list', JFListController::class);
+Route::get('/jf-list', 'JFListController@index');
+Route::get('/jf-list/delete/{id}', 'JFListController@destroy');
