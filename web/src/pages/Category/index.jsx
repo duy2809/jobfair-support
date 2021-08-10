653981b743ad9dc1f4e68821c066c391b8d025d3
MQ <nguyen.minh.quan@sun-asterisk.com>
2021-08-10 14:55:12 +0700

    solved conflict

--- web/src/pages/Category/index.jsx
+++ web/src/pages/Category/index.jsx
@@ -5,16 +5,10 @@ import Navbar from '../../components/navbar'
 
 function App() {
   return (
-    <div>
+    <div className="listCategory">
       <div>
         <Navbar />
       </div>
-      {/* <div className="flex relative">
-                <p className="pt-8 pl-16 font-bold text-4xl">カテゴリー覧</p>
-                <div className="absolute right-12 top-10">
-                    <SearchCategory />
-                </div>
-            </div> */}
       <div>
         <ListCategory />
       </div>
