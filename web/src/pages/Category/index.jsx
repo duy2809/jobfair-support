2990258ba5dfc5b24e931b8a5ca3e7afcb11d6cf
MQ <nguyen.minh.quan@sun-asterisk.com>
2021-08-11 17:02:35 +0700

    review

--- web/src/pages/Category/index.jsx
+++ web/src/pages/Category/index.jsx
@@ -2,16 +2,23 @@
 import 'antd/dist/antd.css'
 import ListCategory from './components/ListCategory'
 import Navbar from '../../components/navbar'
+import OtherLayout from '../../layouts/OtherLayout'
 
 function App() {
   return (
-    <div className="listCategory">
-      <div>
-        <Navbar />
-      </div>
-      <div>
-        <ListCategory />
-      </div>
+    <div>
+      <OtherLayout>
+        <OtherLayout.Main>
+          <div className="listCategory">
+            <div>
+              <Navbar />
+            </div>
+            <div>
+              <ListCategory />
+            </div>
+          </div>
+        </OtherLayout.Main>
+      </OtherLayout>
     </div>
   )
 }
