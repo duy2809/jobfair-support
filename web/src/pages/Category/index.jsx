79094db982f60930ccb572af5e21202588412694
MQ <nguyen.minh.quan@sun-asterisk.com>
2021-08-11 17:07:06 +0700

    review

--- web/src/pages/Category/index.jsx
+++ web/src/pages/Category/index.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/react-in-jsx-scope */
 import 'antd/dist/antd.css'
 import ListCategory from './components/ListCategory'
-import Navbar from '../../components/navbar'
 import OtherLayout from '../../layouts/OtherLayout'
 
 function App() {
@@ -10,12 +9,7 @@ function App() {
       <OtherLayout>
         <OtherLayout.Main>
           <div className="listCategory">
-            <div>
-              <Navbar />
-            </div>
-            <div>
-              <ListCategory />
-            </div>
+            <ListCategory />
           </div>
         </OtherLayout.Main>
       </OtherLayout>
