3c04f94687775d2aa6e8eb3866024fd5aad03409
quynhbh-0153 <bui.huong.quynh@sun-asterisk.com>
2021-08-12 13:45:51 +0700

    g

--- web/src/pages/jobfairs/index.jsx
+++ web/src/pages/jobfairs/index.jsx
@@ -203,13 +203,13 @@ export default function JFList() {
 
   return (
     <div className="JFList">
+
       <OtherLayout>
         <OtherLayout.Main>
+          <h1>JF一覧</h1>
           <div className="container mx-auto flex flex-col space-y-2 justify-center">
             <div className="flex-col space-y-9">
-              <div className="flex items-center">
-                <h1 className="text-3xl float-left">JF一覧</h1>
-              </div>
+              <div className="flex items-center" />
               <div className="space-y-2">
                 <div className="flex justify-between">
                   <div className="flex items-center space-x-3">
