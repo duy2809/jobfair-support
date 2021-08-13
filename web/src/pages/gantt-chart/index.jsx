50b01b492deb67e2a494235c4b07dd835d42c081
VuTan <tanboy042001@gmail.com>
2021-08-13 11:22:24 +0700

    stop build and wait for spec + CL

--- web/src/pages/gantt-chart/index.jsx
+++ web/src/pages/gantt-chart/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import dynamic from 'next/dynamic'
+import { Button } from 'antd'
+import OtherLayout from '../../layouts/OtherLayout'
 
 const DynamicComponentWithNoSSR = dynamic(
+
+  // eslint-disable-next-line import/no-unresolved
   () => import('~/components/gantt-chart/Gantt'),
   { ssr: false },
 )
@@ -20,10 +24,36 @@ const data = {
 
 export default function index() {
   return (
-    <div>
-      <div>
-        <DynamicComponentWithNoSSR tasks={data} />
-      </div>
-    </div>
+    <OtherLayout>
+      <OtherLayout.Main>
+        {/* マイルストーン ガントチャート リストチャート 今日 から まで カテゴリ すべて TC業務  次面接練習 タスクについて  私だけ テンプレート タスクリスト */}
+        <div className="flex justify-between mb-10">
+          <Button type="primary">戻る</Button>
+
+          <Button type="primary" className="tracking-tighter" style={{ letterSpacing: '-2px' }}>タスクリスト</Button>
+        </div>
+        <div className="container mx-auto flex-1 justify-center px-4  pb-20">
+          {/* page title */}
+          <h1 className="text-3xl">JFスケジュール詳細</h1>
+          <div className="gantt-chart">
+            <div>
+              <div className="container xl ">
+                <div className="header">
+                  <Button type="primary">Primary Button</Button>
+                  <Button type="primary">Primary Button</Button>
+                  <Button type="primary">Primary Button</Button>
+                  <Button type="primary">Primary Button</Button>
+
+                </div>
+                <div>
+                  <DynamicComponentWithNoSSR tasks={data} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </OtherLayout.Main>
+    </OtherLayout>
+
   )
 }
