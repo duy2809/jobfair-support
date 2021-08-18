aac384c234e2dd53e74c8d045d030dc3a62a9a18
diemntn-0165 <nguyen.thi.ngoc.diem@sun-asterisk.com>
2021-08-18 09:20:34 +0700

    update api J1

--- api/routes/api.php
+++ api/routes/api.php
@@ -131,5 +131,4 @@ Route::resource('/notification', NotificationController::class);
 Route::get('/notification/delete/{id}', 'NotificationController@destroy');
 
 Route::get('/show-unread/{id}', [App\Http\Controllers\NotificationController::class, 'showUnread']);
-Route::get('/show-notification-user/{id}/{noti_id}', [App\Http\Controllers\NotificationController::class, 'showNotificationUser']);
-Route::get('/show-user-uread/{id}/{noti_id}', [App\Http\Controllers\NotificationController::class, 'showNotificationUserUread']);
+
