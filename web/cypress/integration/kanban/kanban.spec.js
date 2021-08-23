3fadf8b6bfca97ef7db2424a02e111b0c96785fa
donglv-0139 <79511601+donglv-0139@users.noreply.github.com>
2021-08-23 09:49:20 +0700

    Update kanban.spec.js

--- web/cypress/integration/kanban/kanban.spec.js
+++ web/cypress/integration/kanban/kanban.spec.js
@@ -1,69 +1,73 @@
-const { expect } = require("chai");
-const { functionsIn } = require("cypress/types/lodash");
-const { it } = require("mocha");
+// const { expect } = require("chai");
+// const { functionsIn } = require("cypress/types/lodash");
+// const { it } = require("mocha");
 // import "@4tw/cypress-drag-drop";
 
-describe('Check website', function () {
-    it('Visits Kanban', function () {
-      cy.visit('http://jobfair.local:8000/kanban/1');
-    });
-  
-    it('check title'),
+describe('Check kanban', function () {
+  const id = Math.floor(Math.random() * 3) + 1
+  var total = 0
+  var jobfair = []
+  before(() => {
+    // Get information
+    cy.request('GET', `/api/kanban/${id}`).then((res) => {
+      cy.wrap(res.body).as('data')
+      cy.get('@data').then((data) => {
+        total = data.length
+        jobfair = data
+      })
+    })
+  })
+  beforeEach(() => {
+    cy.loginAs('superadmin')
+    cy.visit(`/kanban/${id}`)
+  })
+    
+    it('Check title'),
       () => {
+        cy.get('h1').contains(jobfair[0].jobfairName).should('be','visible')
         cy.get('h1').contains('カンバン').should('be.visible');
       };
   
     it('Check all columns', function () {
       cy.get('.container__column').should('have.length', 5);
     });
-  });
   
-  describe('Check columns', function () {
-    it('column name', function () {
+  
+    it('Check column name', function () {
       const columnItems = ['未着手', '進行中', '完了', '中断', '未完了'];
       cy.get('.container__column').each((item, index, list) => {
         expect(Cypress.$(item).text()).contains(columnItems[index]);
       });
     });
-    //update
-    it('Get data',function(){
-      cy.request({
-        method: 'GET',
-        url : ''
-      }).then(function(response){     //should((response)=>{})
-        cy.log(JSON.stringify(response.body));
-        // cy.location().its('href').
-        // expect().to.eq.(response.body.jobfairName)
-        // expect().to.eq.(response.body.jobfairName)
-      })
-    })
-    //end update
 
-    it('column length', function () {
+    it('Check column length', function () {
       cy.get('.未着手')
-        .children('.container__column--task')
-        .its('length')
-        .should('eq', 5);
+        .then(($未着手)=>{
+          const length = $未着手.children('.container__column--task').its('length');
+          cy.get('.container__column>h3').eq(0).should('include',length);
+        })
       cy.get('.進行中')
-        .children('.container__column--task')
-        .its('length')
-        .should('eq', 1);
+        .then(($進行中)=>{
+          const length = $進行中.children('.container__column--task').its('length');
+          cy.get('.container__column>h3').eq(1).should('include',length);
+        })
       cy.get('.完了')
-        .children('.container__column--task')
-        .its('length')
-        .should('eq', 1);
+        .then(($完了)=>{
+          const length = $完了.children('.container__column--task').its('length');
+          cy.get('.container__column>h3').eq(2).should('include',length);
+        })
       cy.get('.中断')
-        .children('.container__column--task')
-        .its('length')
-        .should('eq', 1);
+        .then(($中断)=>{
+          const length = $中断.children('.container__column--task').its('length');
+          cy.get('.container__column>h3').eq(3).should('include',length);
+        })
       cy.get('.未完了')
-        .children('.container__column--task')
-        .its('length')
-        .should('eq', 2);
+        .then(($未完了)=>{
+          const length = $未完了.children('.container__column--task').its('length');
+          cy.get('.container__column>h3').eq(4).should('include',length);
+        })
     });
-  });
   
-  describe('Check cards', function () {
     it('card info', function () {
       cy.get('.ant-card').should('be.visible');
       cy.get('.ant-card').each(() => {
@@ -77,7 +81,7 @@ describe('Check website', function () {
         .contains('問 題')
         .should('be.visible');
     });
-    it('ard color', function () {
+    it('Check card color', function () {
       cy.get('.ant-card__bordered--未着手').should(
         'have.css',
         'border-left',
@@ -104,21 +108,22 @@ describe('Check website', function () {
         '4px solid rgb(121, 86, 23)'
       );
     });
-  });
-  
-  describe('Check active', function () {
-    // it('task details', function () {
-    //   cy.get('.ant-card-body a').click({ multiple: true });
-    //   cy.url().should('include', '/tasks');
-    // });
+ 
+    it('Check task detail', function () {
+      cy.get('.ant-card-body a').click({ multiple: true });
+      cy.url().should('include', '/task-detail');
+      cy.go('back')
+    });
 
-      it('drag and drop', function(){
+    it('spending to in progress', function(){
         const dataTransfer = new DataTransfer();
-        cy.vist("https://www.seleniumeasy.com/test/drag-and-drop-demo.html");
-        cy.get('#todrag>span:nth-child(2)').trigger('dragstart', {dataTransfer});
-        cy.get('#mydropzone').trigger('drop', {dataTransfer});
-        cy.get('#todrag>span:nth-child(2)').trigger('dragend');        
-        cy.get('#droppedlist span').contains('Draggable 1').should('be.visible');
-      })     
-  });
+        cy.get('.中断:nth-child(1)').trigger('dragstart', {dataTransfer});
+        cy.get('.進行中').trigger('drop', {dataTransfer});
+        cy.get('.中断:nth-child(1').trigger('dragend');  
+        cy.get('.進行中:nth-child(1) button').contains('問 題').should('not.be.visible');
+    }) 
+    
+    
+});
+  
   
