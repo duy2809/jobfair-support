2de7c98d2d1c623129a89ca125cc2ae18fc99c70
diemntn-0165 <nguyen.thi.ngoc.diem@sun-asterisk.com>
2021-08-24 15:48:55 +0700

    fix cypress G1-4

--- web/cypress/support/commands.js
+++ web/cypress/support/commands.js
@@ -15,6 +15,7 @@
  *
  * @param  string role - accept: 'superadmin', 'admin', 'member'
  */
+// import "@4tw/cypress-drag-drop"
 Cypress.Commands.add('loginAs', (role) => {
   let auth = {}
   switch (role) {
@@ -59,3 +60,23 @@ Cypress.Commands.add('loginAs', (role) => {
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 import 'cypress-file-upload'
+//For Cypress drag and drop custom command
+//For Cypress drag and drop plugin
+require('@4tw/cypress-drag-drop')
+Cypress.Commands.add('draganddrop', (dragSelector, dropSelector) => {
+  cy.get(dragSelector).should('exist')
+      .get(dropSelector).should('exist');
+
+  const draggable = Cypress.$(dragSelector)[0]; // Pick up this
+  const droppable = Cypress.$(dropSelector)[0]; // Drop over this
+
+  const coords = droppable.getBoundingClientRect()
+  draggable.dispatchEvent(new MouseEvent('mousedown'));
+  draggable.dispatchEvent(new MouseEvent('mousemove', { clientX: 10, clientY: 0 }));
+  draggable.dispatchEvent(new MouseEvent('mousemove', {
+      clientX: coords.left + 10,
+      clientY: coords.top + 10  // A few extra pixels to get the ordering right
+  }));
+  draggable.dispatchEvent(new MouseEvent('mouseup'));
+  return cy.get(dropSelector);
+})
