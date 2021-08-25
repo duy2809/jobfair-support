e5f141dcadd4f275acb1d89924b26f01a12bae57
minhng-0144 <86640164+minhng-0144@users.noreply.github.com>
2021-08-25 08:36:17 +0700

    Update Assignment.php

--- api/app/Models/Assignment.php
+++ api/app/Models/Assignment.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-protected $fillable = ['notification', 'join_date', 'completed_date', 'user_id', 'task_id'];
 class Assignment extends Model
 {
     use HasFactory;
+    
+    protected $fillable = ['notification', 'join_date', 'completed_date', 'user_id', 'task_id'];
 }
