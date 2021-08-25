161674dbbe0bbcb359a6f248818640cbcf81a68c
DoTuanHoang <tuanhoang2do2311@gmail.com>
2021-08-25 16:54:07 +0700

    fix CI error

--- api/app/Http/Controllers/TaskController.php
+++ api/app/Http/Controllers/TaskController.php
@@ -7,7 +7,6 @@ use App\Models\Schedule;
 use App\Models\Task;
 use App\Models\TemplateTask;
 use Carbon\Carbon;
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\Rule;
