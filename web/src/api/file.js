6af2f25acd78c2829650e1bd9f5c4ce404a7f378
NguyenTu <nguyen.bach.anh.tu@sun-asterisk.com>
2021-08-26 21:05:06 +0700

    fix api

--- web/src/api/file.js
+++ web/src/api/file.js
@@ -5,9 +5,6 @@ export const getRootPathFile = async (JFid) => instance.get(`/file/${JFid}`)
 export const getLatest = async () => instance.get('/file/getLatest')
 export const deleteDocument = async (id, argument) => instance.post(`file/${id}/delArray`, argument)
 export const getPath = async (argument) => instance.get('file/getPath', argument)
-<<<<<<< HEAD
-=======
 export const getMember = async (id) => instance.get(`${id}/member`)
 export const searchFile = async (argument) => instance.get('file/find', argument)
 export const addDocument = (agr) => instance.post('/file', agr)
->>>>>>> f653ac57a43d5efce7e8a83caf578734f6c351a0
