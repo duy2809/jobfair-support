6f2286bf66e642d4386dd0d25ffbf6807bb3613c
minhtan4711 <minhtan471100@gmail.com>
2021-08-27 10:23:10 +0700

    fix ui in progess

--- web/src/components/list/index.jsx
+++ web/src/components/list/index.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState, useRef } from 'react'
-import { Button, Table, Input, DatePicker } from 'antd'
-import { PlusOutlined, SearchOutlined } from '@ant-design/icons'
-import Link from 'next/link'
-import PropTypes from 'prop-types'
-import { taskSearch } from '../../api/top-page'
+import React, { useEffect, useState, useRef } from "react";
+import { Button, Table, Input, DatePicker } from "antd";
+import { PlusOutlined, SearchOutlined } from "@ant-design/icons";
+import Link from "next/link";
+import PropTypes from "prop-types";
+import { taskSearch } from "../../api/top-page";
 
 // const { Search } = Input;
 
@@ -19,130 +19,144 @@ const List = ({
   route,
   routeToAdd,
 }) => {
-  const ref = useRef()
-  const [show, setShow] = useState(false)
-  const [showSearchIcon, setShowSearchIcon] = useState(searchIcon)
-  const [list, setList] = useState([])
-  const [filter, setFilter] = useState(() => ({ name: '', milestone: '', category: '', date: '' }))
+  const ref = useRef();
+  const [show, setShow] = useState(false);
+  const [showSearchIcon, setShowSearchIcon] = useState(searchIcon);
+  const [list, setList] = useState([]);
+  const [filter, setFilter] = useState(() => ({
+    name: "",
+    milestone: "",
+    category: "",
+    date: "",
+  }));
 
   useEffect(() => {
-    setList(dataSource)
-  }, [dataSource])
+    setList(dataSource);
+  }, [dataSource]);
 
   useEffect(() => {
     const onBodyClick = (event) => {
       if (ref.current.contains(event.target)) {
-        return
+        return;
       }
-      setShow(false)
-      setShowSearchIcon(true)
-    }
+      setShow(false);
+      setShowSearchIcon(true);
+    };
 
-    document.body.addEventListener('click', onBodyClick, { capture: true })
+    document.body.addEventListener("click", onBodyClick, { capture: true });
 
     return () => {
-      document.body.removeEventListener('click', onBodyClick, {
+      document.body.removeEventListener("click", onBodyClick, {
         capture: true,
-      })
-    }
-  }, [])
+      });
+    };
+  }, []);
   useEffect(() => {
-    let datas = [...list]
-    console.log(filter)
+    let datas = [...list];
+    console.log(filter);
     if (filter) {
       if (filter.name) {
-        console.log('fil name')
+        console.log("fil name");
         datas = datas.filter(
-          (data) => data.name.toLowerCase().indexOf(filter.name.toLowerCase()) !== -1,
-        )
+          (data) =>
+            data.name.toLowerCase().indexOf(filter.name.toLowerCase()) !== -1
+        );
       }
       if (filter.milestone) {
-        console.log('fil mil')
+        console.log("fil mil");
         datas = datas.filter(
-          (data) => data.milestone.toLowerCase().indexOf(filter.milestone.toLowerCase()) !== -1,
-        )
+          (data) =>
+            data.milestone
+              .toLowerCase()
+              .indexOf(filter.milestone.toLowerCase()) !== -1
+        );
       }
       if (filter.category) {
-        console.log('fil cate')
+        console.log("fil cate");
         datas = datas.filter(
-          (data) => data.category.toLowerCase().indexOf(filter.category.toLowerCase()) !== -1,
-        )
+          (data) =>
+            data.category
+              .toLowerCase()
+              .indexOf(filter.category.toLowerCase()) !== -1
+        );
       }
       if (filter.date) {
-        if (dataColumn[1].dataIndex === 'type') filter.date = filter.date.replace('-', '/')
+        if (dataColumn[1].dataIndex === "type")
+          filter.date = filter.date.replace("-", "/");
         datas = datas.filter(
-          (data) => data.time.toLowerCase().indexOf(filter.date.toLowerCase()) !== -1,
-        )
+          (data) =>
+            data.time.toLowerCase().indexOf(filter.date.toLowerCase()) !== -1
+        );
       }
-      setList(datas)
+      setList(datas);
     }
-  }, [filter])
+  }, [filter]);
   const onClick = () => {
-    setShow(!show)
-    setShowSearchIcon(!showSearchIcon)
-  }
+    setShow(!show);
+    setShowSearchIcon(!showSearchIcon);
+  };
 
-  const searchInput = (e, dateString = '') => {
+  const searchInput = (e, dateString = "") => {
     if (!dateString) {
-      console.log(e)
-      if (e.target.name === 'name') {
-        setFilter({ ...filter, name: e.target.value })
-        if (e.target.value === '') {
-          setFilter({ ...filter, name: '' })
-          setList(dataSource)
+      console.log(e);
+      if (e.target.name === "name") {
+        setFilter({ ...filter, name: e.target.value });
+        if (e.target.value === "") {
+          setFilter({ ...filter, name: "" });
+          setList(dataSource);
         }
       }
-      if (e.target.name === 'milestone') {
-        setFilter({ ...filter, milestone: e.target.value })
-        if (e.target.value === '') {
-          setFilter({ ...filter, milestone: '' })
-          setList(dataSource)
+      if (e.target.name === "milestone") {
+        setFilter({ ...filter, milestone: e.target.value });
+        if (e.target.value === "") {
+          setFilter({ ...filter, milestone: "" });
+          setList(dataSource);
         }
       }
-      if (e.target.name === 'category') {
-        setFilter({ ...filter, category: e.target.value })
-        if (e.target.value === '') {
-          setFilter({ ...filter, category: '' })
-          setList(dataSource)
+      if (e.target.name === "category") {
+        setFilter({ ...filter, category: e.target.value });
+        if (e.target.value === "") {
+          setFilter({ ...filter, category: "" });
+          setList(dataSource);
         }
       }
     } else {
-      setFilter({ ...filter, date: dateString })
-      if (dateString === '') {
-        setFilter({ ...filter, date: '' })
-        setList(dataSource)
+      setFilter({ ...filter, date: dateString });
+      if (dateString === "") {
+        setFilter({ ...filter, date: "" });
+        setList(dataSource);
       }
     }
-  }
+  };
   const searchByJobfairName = (e) => {
     const getTask = async () => {
-      const response = await taskSearch(e.target.value)
-      let tasks = []
+      const response = await taskSearch(e.target.value);
+      let tasks = [];
       tasks = response.data.map((data) => ({
         name: data.name,
         jfName: data.jobfair.name,
         time: data.start_time,
-      }))
-      setList(tasks)
-    }
-    getTask()
-  }
+      }));
+      setList(tasks);
+    };
+    getTask();
+  };
   return (
     <div ref={ref}>
       <div
         style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          marginBottom: '10px',
+          display: "flex",
+          justifyContent: "space-between",
+          marginBottom: "10px",
         }}
       >
         <Link href={route}>
           <a
             style={{
-              fontSize: '30px',
-              whiteSpace: 'nowrap',
-              overflow: 'hidden',
-              textOverflow: 'ellipsis',
+              fontSize: "30px",
+              whiteSpace: "nowrap",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
             }}
           >
             {text}
@@ -150,12 +164,12 @@ const List = ({
         </Link>
 
         <div className="flex items-center">
-          {text === 'タスク一覧' ? null : (
+          {text === "タスク一覧" ? null : (
             <Link href={routeToAdd}>
               <Button
-                style={{ border: 'none', marginBottom: '5px' }}
+                style={{ border: "none", marginBottom: "5px" }}
                 shape="circle"
-                icon={<PlusOutlined style={{ fontSize: '30px' }} />}
+                icon={<PlusOutlined style={{ fontSize: "30px" }} />}
               />
             </Link>
           )}
@@ -163,9 +177,13 @@ const List = ({
           <span className="queue-demo">
             {showSearchIcon && (
               <Button
-                style={{ border: 'none' }}
+                style={{ border: "none" }}
                 shape="circle"
-                icon={<SearchOutlined style={{ marginLeft: '4px', fontSize: '30px' }} />}
+                icon={
+                  <SearchOutlined
+                    style={{ marginLeft: "4px", fontSize: "30px" }}
+                  />
+                }
                 onClick={onClick}
               />
             )}
@@ -189,17 +207,17 @@ const List = ({
 
       <div
         style={{
-          display: 'grid',
-          gridTemplateRows: '15% 75%',
-          height: '480px',
-          backgroundColor: 'white',
-          border: '1px solid black',
-          borderRadius: '10px',
+          display: "grid",
+          gridTemplateRows: "15% 75%",
+          height: "480px",
+          backgroundColor: "white",
+          border: "1px solid black",
+          borderRadius: "10px",
         }}
       >
         <div
           style={{
-            display: 'grid',
+            display: "grid",
           }}
         >
           <div className="flex items-center justify-end px-2">
@@ -263,11 +281,6 @@ const List = ({
         {/* Table data */}
         <div>
           <Table
-            // pagination={{
-            //   position: ["bottomCenter"],
-            //   responsive: true,
-            //   defaultPageSize: 5,
-            // }}
             scroll={{ y: 280 }}
             pagination={false}
             dataSource={list}
@@ -276,8 +289,8 @@ const List = ({
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 List.propTypes = {
   searchIcon: PropTypes.bool.isRequired,
@@ -290,6 +303,6 @@ List.propTypes = {
   dataSource: PropTypes.array.isRequired,
   route: PropTypes.string.isRequired,
   routeToAdd: PropTypes.string.isRequired,
-}
+};
 
-export default List
+export default List;
