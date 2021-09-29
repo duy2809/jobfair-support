e1d2bd616e45c28b3a31d9e2fa21e0954b940940
nhdang0134 <nguyen.hai.dang-b@sun-asterisk.com>
2021-09-29 08:08:08 +0700

    fix label

--- web/src/pages/jf-schedule/add/index.jsx
+++ web/src/pages/jf-schedule/add/index.jsx
@@ -151,104 +151,106 @@ function addJobfairSchedule() {
   return (
     <Layout>
       <Layout.Main>
-        <h1>JFスケジュール登録</h1>
-        <Form
-          labelAlign="left"
-          labelCol={{ span: 7 }}
-          size="large"
-          form={form}
-          name="edit-jfschedule"
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          requiredMark="optional"
-        >
-          <div className="w-1/2">
-            <Form.Item
-              label={(
-                <div className="flex items-center justify-between">
-                  <ScheduleOutlined style={{ fontSize: '32px' }} />
-                  <span className="ml-2">JFスケジュール名</span>
-                </div>
-              )}
-              name="jfschedule_name"
-              rules={[
-                {
-                  required: true,
-                  message: 'JFスケジュール名を入力してください。',
-                },
-              ]}
-            >
-              <Input
-                placeholder="JFスケジュール名を入力してください。"
-                onChange={onValueNameChange}
-                onBlur={onBlur}
-              />
-            </Form.Item>
-            <Form.Item
-              label={(
-                <div className="flex items-center w-full">
-                  <FlagOutlined style={{ fontSize: '32px' }} />
-                  <span className="ml-2">マイルストーン</span>
-                </div>
-              )}
-              name="milestone_select"
-              rules={[
-                {
-                  required: true,
-                  message: 'マイルストーンを入力してください。',
-                },
-              ]}
-            >
-              <Select {...selectMilestoneProps} />
-            </Form.Item>
-          </div>
-          <Divider />
-          <Row gutter={[24, 24]}>
-            {dataList.map((milestone) => {
-              const templateTaskChildernList = _.filter(templateTaskList, {
-                milestone_id: milestone.id,
-              })
-              const templateTaskOptions = []
-              templateTaskChildernList.forEach((item) => {
-                const value = item.id
-                templateTaskOptions.push({
-                  label: item.name,
-                  value,
-                })
-              })
-              const addedTemplateTaskChildernList = []
-              templateTaskChildernList.forEach((item) => {
-                if (_.includes(addedTemplateTaskList, item.id)) {
-                  addedTemplateTaskChildernList.push(item.id)
-                }
-              })
-              return (
-                <Col span={12} key={milestone.id}>
-                  <List
-                    milestone={milestone}
-                    templateTaskChildernList={templateTaskChildernList}
-                    addedTemplateTaskChildernList={addedTemplateTaskChildernList}
-                    templateTaskOptions={templateTaskOptions}
-                    onDeleteTemplateTask={onDeleteTemplateTask}
-                    onDeleteMilestone={onDeleteMilestone}
-                    onAddTemplateTask={onAddTemplateTask}
-                    selectName={`template_task_select_${milestone.id}`}
-                    form={form}
-                  />
-                </Col>
-              )
-            })}
-          </Row>
-
-          <Form.Item>
-            <div className="mt-5 flex justify-end">
-              <CancelBtn />
-              <Button type="primary" htmlType="submit" className="ml-3">
-                登録
-              </Button>
+        <div className="add-jf-schedule">
+          <h1>JFスケジュール登録</h1>
+          <Form
+            labelAlign="left"
+            labelCol={{ span: 7 }}
+            size="large"
+            form={form}
+            name="edit-jfschedule"
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            requiredMark="optional"
+          >
+            <div className="w-1/2">
+              <Form.Item
+                label={(
+                  <div className="flex items-center justify-between">
+                    <ScheduleOutlined style={{ fontSize: '32px' }} />
+                    <span className="ml-2 font-bold">JFスケジュール名</span>
+                  </div>
+                )}
+                name="jfschedule_name"
+                rules={[
+                  {
+                    required: true,
+                    message: 'JFスケジュール名を入力してください。',
+                  },
+                ]}
+              >
+                <Input
+                  placeholder="JFスケジュール名を入力してください。"
+                  onChange={onValueNameChange}
+                  onBlur={onBlur}
+                />
+              </Form.Item>
+              <Form.Item
+                label={(
+                  <div className="flex items-center w-full">
+                    <FlagOutlined style={{ fontSize: '32px' }} />
+                    <span className="ml-2 font-bold">マイルストーン</span>
+                  </div>
+                )}
+                name="milestone_select"
+                rules={[
+                  {
+                    required: true,
+                    message: 'マイルストーンを入力してください。',
+                  },
+                ]}
+              >
+                <Select {...selectMilestoneProps} />
+              </Form.Item>
             </div>
-          </Form.Item>
-        </Form>
+            <Divider />
+            <Row gutter={[24, 24]}>
+              {dataList.map((milestone) => {
+                const templateTaskChildernList = _.filter(templateTaskList, {
+                  milestone_id: milestone.id,
+                })
+                const templateTaskOptions = []
+                templateTaskChildernList.forEach((item) => {
+                  const value = item.id
+                  templateTaskOptions.push({
+                    label: item.name,
+                    value,
+                  })
+                })
+                const addedTemplateTaskChildernList = []
+                templateTaskChildernList.forEach((item) => {
+                  if (_.includes(addedTemplateTaskList, item.id)) {
+                    addedTemplateTaskChildernList.push(item.id)
+                  }
+                })
+                return (
+                  <Col span={12} key={milestone.id}>
+                    <List
+                      milestone={milestone}
+                      templateTaskChildernList={templateTaskChildernList}
+                      addedTemplateTaskChildernList={addedTemplateTaskChildernList}
+                      templateTaskOptions={templateTaskOptions}
+                      onDeleteTemplateTask={onDeleteTemplateTask}
+                      onDeleteMilestone={onDeleteMilestone}
+                      onAddTemplateTask={onAddTemplateTask}
+                      selectName={`template_task_select_${milestone.id}`}
+                      form={form}
+                    />
+                  </Col>
+                )
+              })}
+            </Row>
+
+            <Form.Item>
+              <div className="mt-5 flex justify-end">
+                <CancelBtn />
+                <Button type="primary" htmlType="submit" className="ml-3">
+                  登録
+                </Button>
+              </div>
+            </Form.Item>
+          </Form>
+        </div>
       </Layout.Main>
     </Layout>
   )
