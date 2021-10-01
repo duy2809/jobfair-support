09aeaa1a72f935db05e638b1852f52d180d5baff
luctq-0138 <tran.quoc.luc@sun-asterisk.com>
2021-10-01 09:32:59 +0700

    fix UI List jobfair

--- web/src/api/jf-list.js
+++ web/src/api/jf-list.js
@@ -1,4 +1,4 @@
 import axios from './axios'
 
 export const getJFList = () => axios.get('/jobfair')
-export const deleteJFList = async (id) => axios.get(`/jf-list/delete/${id}`)
+export const deleteJF = (id) => axios.delete(`/jobfair/${id}`)
