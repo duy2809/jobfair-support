beb9f0864edef478335b4bd70a1394b13a7c2c84
quynhbh-0153 <bui.huong.quynh@sun-asterisk.com>
2021-10-01 10:24:20 +0700

    fix

--- web/src/pages/template-tasks/index.jsx
+++ web/src/pages/template-tasks/index.jsx
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from 'react'
-import { Table, Input, Empty, Select, Tooltip, Button } from 'antd'
+import { Table, Input, Empty, Popover, Modal, Select, notification, Tooltip, Button, Space } from 'antd'
 import './style.scss'
-import { SearchOutlined, UpOutlined, DownOutlined } from '@ant-design/icons'
+import { SearchOutlined, CheckCircleTwoTone, FilterOutlined, ExclamationCircleOutlined, EditTwoTone, DeleteTwoTone } from '@ant-design/icons'
+import { useRouter } from 'next/router'
 import OtherLayout from '../../layouts/OtherLayout'
-import { getTaskList, getCategories } from '../../api/template-task'
+import { getTaskList, getCategories, deleteTptt } from '../../api/template-task'
 import { getAllMileStone } from '../../api/milestone'
 import { webInit } from '../../api/web-init'
 
 function TemplateTaskList() {
   // state of table
+  const [isFilter, setIsFilter] = useState(false)
+  const [visible, setVisible] = useState(false)
+  const router = useRouter()
   const [users, setUsers] = useState('')
   const [itemCount, setItemCount] = useState(10)
   const [pagination, setPagination] = useState({ position: ['bottomCenter'], showTitle: false, showSizeChanger: false, pageSize: 10 })
@@ -81,17 +85,49 @@ function TemplateTaskList() {
     }
     setOptionMileStone(option)
   }
-
+  const handleEdit = (id) => {
+    router.push(`/template-tasks/${id}/edit`)
+  }
+  const handleVisibleChange = () => {
+    setVisible(!visible)
+  }
+  const saveNotification = () => {
+    notification.open({
+      icon: <CheckCircleTwoTone twoToneColor="#52c41a" />,
+      duration: 3,
+      message: '正常に削除されました',
+      onClick: () => {},
+    })
+  }
+  const deletetpl = async (id) => {
+    const newList = temperaryData.filter((x) => x.idTemplateTask !== id)
+    setTemperaryData(newList)
+    await deleteTptt(id)
+      .then((response) => {
+        console.log(response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    saveNotification()
+  }
+  const modelDelete = (id) => {
+    Modal.confirm({
+      title: '削除してもよろしいですか？',
+      icon: <ExclamationCircleOutlined />,
+      content: '',
+      onOk: () => {
+        deletetpl(id)
+      },
+      onCancel: () => {},
+      centered: true,
+      okText: 'はい',
+      cancelText: 'いいえ',
+    })
+  }
   // columns of tables
 
-  const columns = [
-    {
-      title: 'No.',
-      dataIndex: 'id',
-      fixed: 'left',
-      width: '5%',
-      render: (id) => id,
-    },
+  const columns = users === 'superadmin' ? [
     {
       title: 'テンプレートタスク名',
       dataIndex: 'templateTaskName',
@@ -105,10 +141,63 @@ function TemplateTaskList() {
       title: 'カテゴリ',
       dataIndex: 'category_name',
       fixed: 'left',
+      render: (taskName, record) => (
+        <a href={`/template-task-dt/${record.idTemplateTask}`}>{taskName}</a>
+      ),
     },
     {
       title: 'マイルストーン',
       dataIndex: 'milestone_name',
+      render: (taskName, record) => (
+        <a href={`/template-task-dt/${record.idTemplateTask}`}>{taskName}</a>
+      ),
+    },
+    {
+      title: 'アクション',
+      key: 'action',
+      width: '10%',
+      render: (_text, record) => users === 'superadmin' && (
+        <Space size="middle">
+          <EditTwoTone
+            id={record.id}
+            onClick={() => {
+              handleEdit(record.idTemplateTask)
+            }}
+          />
+
+          <DeleteTwoTone
+            id={record.id}
+            onClick={() => {
+              modelDelete(record.idTemplateTask)
+            }}
+          />
+        </Space>
+      ),
+    },
+  ] : [
+    {
+      title: 'テンプレートタスク名',
+      dataIndex: 'templateTaskName',
+      fixed: 'left',
+      ellipsis: {
+        showTitle: false,
+      },
+      render: (templateTaskName, record) => <Tooltip title={templateTaskName}><a href={`/template-task-dt/${record.idTemplateTask}`}>{templateTaskName}</a></Tooltip>,
+    },
+    {
+      title: 'カテゴリ',
+      dataIndex: 'category_name',
+      fixed: 'left',
+      render: (taskName, record) => (
+        <a href={`/template-task-dt/${record.idTemplateTask}`}>{taskName}</a>
+      ),
+    },
+    {
+      title: 'マイルストーン',
+      dataIndex: 'milestone_name',
+      render: (taskName, record) => (
+        <a href={`/template-task-dt/${record.idTemplateTask}`}>{taskName}</a>
+      ),
     },
   ]
 
@@ -150,6 +239,9 @@ function TemplateTaskList() {
   }
 
   const handleSelectCategory = (value) => {
+    if (value) {
+      setIsFilter(true)
+    } else setIsFilter(false)
     setCategory(value)
     const filteredData = originalData.filter(
       (templateTask) => (value ? !templateTask.category_name.localeCompare(value) : templateTask.category_name)
@@ -160,6 +252,9 @@ function TemplateTaskList() {
   }
 
   const handlSelectMilestone = (value) => {
+    if (value) {
+      setIsFilter(true)
+    } else setIsFilter(false)
     setMilestone(value)
     const filteredData = originalData.filter(
       (templateTask) => (value ? !templateTask.milestone_name.localeCompare(value) : templateTask.milestone_name)
@@ -168,7 +263,6 @@ function TemplateTaskList() {
     )
     setTemperaryData(filteredData)
   }
-  const [showFilter, setShowFilter] = useState(true)
   return (
     <OtherLayout>
       <OtherLayout.Main>
@@ -179,53 +273,11 @@ function TemplateTaskList() {
                 <div className="flex items-center">
                   <h1 className="text-3xl float-left">テンプレートタスクー覧</h1>
                 </div>
-                <div className="space-y-2">
-                  <div className="flex items-center justify-between">
-                    <div className="flex items-center font-semibold">
-                      {showFilter ? (
-                        <UpOutlined
-                          className="icon"
-                          onClick={() => {
-                            setShowFilter(!showFilter)
-                          }}
-                        />
-                      ) : (
-                        <DownOutlined
-                          className="icon"
-                          onClick={() => {
-                            setShowFilter(!showFilter)
-                          }}
-                        />
-                      )}
-                      <div>&nbsp;フィルタ</div>
-                    </div>
-                    {users === 'superadmin' ? (
-                      <>
-                        <Button
-                          className="float-right"
-                          href="/add-template-task"
-                          type="primary"
-                          style={{ letterSpacing: '-2px' }}
-                        >
-                          追加
-                        </Button>
-                      </>
-                    )
-                      : null}
-                  </div>
-                  <div className="flex items-center space-x-4 w-9/12" style={{ display: showFilter ? '' : 'none' }}>
-                    <Select className="w-1/4" placeholder="カテゴリ" allowClear="true" onChange={handleSelectCategory}>
-                      {optionCategory}
-                    </Select>
-                    <Select className="w-1/4" placeholder="マイルストーン" allowClear="true" onChange={handlSelectMilestone}>
-                      {optionMilestone}
-                    </Select>
-                  </div>
-                </div>
+
               </div>
               <div className="flex items-center justify-between">
                 <div>
-                  <span>表示件数: </span>
+                  <span>表示件数 </span>
                   <Select value={itemCount} onChange={handleSelect}>
                     <Option value={10}>10</Option>
                     <Option value={25}>25</Option>
@@ -233,21 +285,72 @@ function TemplateTaskList() {
                   </Select>
                 </div>
                 <div>
-                  <Input
-                    className="float-right"
-                    allowClear="true"
-                    prefix={<SearchOutlined />}
-                    placeholder="テンプレートタスク名"
-                    onChange={onSearch}
-                    value={valueSearch}
-                  />
+                  <div className="searchAdd">
+                    <Popover
+                      content={(
+                        <>
+                          <h6 className="mb-1" style={{ fontWeight: 700 }}>カテゴリ</h6>
+
+                          <Select style={{ width: '300px' }} className="w-1/4" placeholder="カテゴリ" allowClear="true" onChange={handleSelectCategory}>
+                            {optionCategory}
+                          </Select>
+
+                          <h6 className="mb-1 mt-2" style={{ fontWeight: 700 }}>マイルストーン </h6>
+
+                          <Select style={{ width: '300px' }} className="w-1/4" placeholder="マイルストーン" allowClear="true" onChange={handlSelectMilestone}>
+                            {optionMilestone}
+                          </Select>
+
+                        </>
+
+                      )}
+                      className="mr-2"
+                      placement="bottomLeft"
+                      trigger="click"
+                      visible={visible}
+                      onVisibleChange={handleVisibleChange}
+                    >
+                      {visible || isFilter ? (
+                        <Button
+                          shape="circle"
+                          style={{ color: '#ffd803' }}
+                          icon={<FilterOutlined id="filter" />}
+                        />
+                      ) : (
+                        <Button
+                          shape="circle"
+                          icon={<FilterOutlined id="filter" />}
+                        />
+                      )}
+                    </Popover>
+                    <Input
+                      className="float-right mr-3"
+                      allowClear="true"
+                      prefix={<SearchOutlined />}
+                      placeholder="テンプレートタスク名"
+                      onChange={onSearch}
+                      value={valueSearch}
+                    />
+                    {users === 'superadmin' ? (
+                      <>
+                        <Button
+                          className="float-right"
+                          href="/add-template-task"
+                          type="primary"
+                        >
+                          <span> 追加 </span>
+                        </Button>
+                      </>
+                    )
+                      : null}
+                  </div>
+
                 </div>
               </div>
             </div>
             <Table
               columns={columns}
               dataSource={temperaryData}
-              scroll={{ y: 400 }}
               loading={loading}
               pagination={pagination}
               locale={{ emptyText: <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description="該当結果が見つかりませんでした" /> }}
