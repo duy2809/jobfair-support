cb7f8b18956971b0b9cd8f0230d51d541fedc448
minhng-0144 <nguyen.gia.minh@sun-asterisk.com>
2021-10-06 13:57:22 +0700

    add contrain

--- api/database/migrations/2021_10_05_095733_create_comments_table.php
+++ api/database/migrations/2021_10_05_095733_create_comments_table.php
@@ -18,6 +18,8 @@ class CreateCommentsTable extends Migration
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('task_id');
             $table->text('body');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('task_id')->references('id')->on('tasks')->onDelete('cascade');
             $table->timestamps();
         });
     }
