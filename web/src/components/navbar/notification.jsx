b89613a1b49cddab983932163a603816db11771c
diemntn-05102000 <ngocdiem5102000@gmail.com>
2021-10-29 09:44:12 +0700

    fix noti contronller and frontend

--- web/src/components/navbar/notification.jsx
+++ web/src/components/navbar/notification.jsx
@@ -16,6 +16,7 @@ export default function Notification() {
   // const [lengthNoti, setLengthNoti] = useState()
 
   const [user, setUser] = useState(null)
+  const [userId, setUserId] = useState(null)
   const [unread, setUnRead] = useState(false)
   const [unreadLength, setUnReadLength] = useState(0)
   const { store } = useContext(ReactReduxContext)
@@ -32,6 +33,7 @@ export default function Notification() {
       setUser(store.getState().get('auth').get('user'))
       if (user) {
         const id = user.get('id')
+        setUserId(id)
         let data
         if (unread) {
           const res = await getUnreadNotification(id)
@@ -167,26 +169,12 @@ export default function Notification() {
     }
   }
 
-  // const getNoti = (value) => {
-  //   console.log(value)
-  // }
-
-  // change ...
-  // function handleChange(value) {
-  //   console.log(`Selected: ${value}`)
-  //   getNoti(value)
-  // }
-
-  // show noti
   const [visible, setVisible] = useState(false)
 
   const handleVisibleChange = () => {
     setVisible(!visible)
   }
   const deleteNoti = (notiId) => {
-    // console.log(noti_id)
-    // setDeleteNoti(noti_id)
-    // console.log(deleteNotiID)
     deleteNotification(notiId).then((res) => {
       if (res.data == null) {
         return
@@ -214,16 +202,8 @@ export default function Notification() {
     })
   }
 
-  const handlerClick = (type, id) => {
-    if (type === 'タスク') {
-      window.location.href = `/task-detail/${id}`
-    }
-    if (type === 'メンバ') {
-      window.location.href = `/member/${id}`
-    }
-    if (type === 'JF') {
-      window.location.href = `/jf-toppage/${id}`
-    }
+  const handlerClick = (url) => {
+    window.location.href = url
   }
 
   // const convertDate = (date) => {
