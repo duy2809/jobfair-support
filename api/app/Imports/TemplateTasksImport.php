a9dbe4c98cdeae99e8355d47da77e40ea89b152a
Phong Vu UET <vuphonghyuet@gmail.com>
2021-11-04 11:26:15 +0700

    fix CI

--- api/app/Imports/TemplateTasksImport.php
+++ api/app/Imports/TemplateTasksImport.php
@@ -37,16 +37,16 @@ class TemplateTasksImport implements ToModel
         $milestone = $this->milestones->where('name', trim($row[1]))->first();
         $arr = explode(',', $row[2]);
         $category = $this->categories->whereIn('category_name', $arr)->pluck('id');
-        $row[4] = "\n" . $row[4];
-        $description = $row[3] . " \n" . preg_replace('/\n・/', "\n" . '- [ ] ', $row[4]);
+        $row[4] = "\n".$row[4];
+        $description = $row[3]." \n".preg_replace('/\n・/', "\n".'- [ ] ', $row[4]);
         //$description = substr($description, 0, strlen($row[3]) + 1).substr($description, strlen($row[3]) + 9);
         $newTemplateTask = TemplateTask::create([
-            'name' => trim($row[0]),
+            'name'                  => trim($row[0]),
             'description_of_detail' => $description,
-            'milestone_id' => $milestone->id ?? null,
-            'is_day' => $row[6] === '時間' ? 0 : 1,
-            'unit' => $unit,
-            'effort' => $row[5],
+            'milestone_id'          => $milestone->id ?? null,
+            'is_day'                => $row[6] === '時間' ? 0 : 1,
+            'unit'                  => $unit,
+            'effort'                => $row[5],
         ]);
         $newTemplateTask->categories()->attach($category);
 
