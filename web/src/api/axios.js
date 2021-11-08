c6cd822d22dbf9407108b713863a12ccdc850beb
quynhnv-0136 <nguyen.van.quynh@sun-asterisk.com>
2021-11-08 17:33:54 +0700

    fix CI build error

--- web/src/api/axios.js
+++ web/src/api/axios.js
@@ -5,7 +5,7 @@ import cookies from 'axios/lib/helpers/cookies'
 const instance = axios.create({
   baseURL: process.browser
     ? process.env.BROWSER_API_URL
-    : require('../../config/next').SERVER_API_URL,
+    : process.env.SERVER_API_URL,
   headers: {
     'X-XSRF-TOKEN': cookies.read('XSRF-TOKEN'),
   },
