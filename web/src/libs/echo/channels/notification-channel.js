0b6108f9be5db6d00640afa58576becb0eec10eb
diemntn-05102000 <ngocdiem5102000@gmail.com>
2021-11-09 15:31:45 +0700

    fix conflict

--- web/src/libs/echo/channels/notification-channel.js
+++ web/src/libs/echo/channels/notification-channel.js
@@ -3,11 +3,7 @@ import echo from '../connection'
 export default class NotificationChannel {
   constructor(userId) {
     this.userId = userId
-<<<<<<< HEAD
     this.channel = echo.private(`App.Models.User.${userId}`)
-=======
-    this.channel = echo.channel(`laravel_database_private-App.Models.User.${userId}`)
->>>>>>> test notification realtime
     this.onOutputCb = null
   }
 
