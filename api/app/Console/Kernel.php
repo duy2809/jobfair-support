f7649bf81fba2ccb8b9305a3e122f1aca95d4d05
minhng-0144 <nguyen.gia.minh@sun-asterisk.com>
2021-11-17 16:35:42 +0700

    add file kernel

--- api/app/Console/Kernel.php
+++ api/app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        \App\Console\Commands\UpdateTask::class
+        \App\Console\Commands\UpdateTask::class,
     ];
 
     /**
