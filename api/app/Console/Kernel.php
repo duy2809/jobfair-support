5c9516c88f4e52c0869e44c5cd9e2db764813af6
Phong Vu UET <vuphonghyuet@gmail.com>
2021-11-19 09:38:06 +0700

    feat(DeleteReadNotification): add daily task scheduler

--- api/app/Console/Kernel.php
+++ api/app/Console/Kernel.php
@@ -25,6 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('task:update')->daily();
+        $schedule->command('notification:delete')->daily();
     }
 
     /**
