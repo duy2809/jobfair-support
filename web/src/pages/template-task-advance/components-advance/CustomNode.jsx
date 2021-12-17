08ee221fea246cda4c650d76e0057696751db7cb
quynhbh-0153 <bui.huong.quynh@sun-asterisk.com>
2021-12-17 12:35:34 +0700

    fixUI

--- web/src/pages/template-task-advance/components-advance/CustomNode.jsx
+++ web/src/pages/template-task-advance/components-advance/CustomNode.jsx
@@ -1,7 +1,8 @@
+/* eslint-disable no-nested-ternary */
 /* eslint-disable import/no-duplicates */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import React, { useState } from "react";
+import React, { useState } from 'react'
 // import IconButton from '@material-ui/core/IconButton'
 // import { ArrowRight, Delete, FileCopy } from '@material-ui/icons'
 import {
@@ -15,58 +16,57 @@ import {
   SearchOutlined,
   DownOutlined,
   RightOutlined,
-} from "@ant-design/icons";
-import { Row, Col, Slider } from "antd";
-import { useDragOver } from "@minoru/react-dnd-treeview";
+} from '@ant-design/icons'
+import { Row, Col, Slider } from 'antd'
+import { useDragOver } from '@minoru/react-dnd-treeview'
 import {
   DeleteTwoTone,
   // eslint-disable-next-line import/no-duplicates
-} from "@ant-design/icons";
-import { TypeIcon } from "./TypeIcon";
-import styles from "./CustomNode.module.scss";
+} from '@ant-design/icons'
+import { TypeIcon } from './TypeIcon'
+import styles from './CustomNode.module.scss'
 
 export const CustomNode = (props) => {
-  const [hover, setHover] = useState(false);
-  const { id, parent, text } = props.node;
-  const indent = props.depth * 24;
-  const [labelText, setLabelText] = useState(text);
-  const [iseOpen, setIsOpen] = useState(false);
-  const [visibleInput, setVisibleInput] = useState(false);
+  const [hover, setHover] = useState(false)
+  const { id, parent, text } = props.node
+  const indent = props.depth * 24
+  const [labelText, setLabelText] = useState(text)
+  const [iseOpen, setIsOpen] = useState(false)
+  const [visibleInput, setVisibleInput] = useState(false)
   const handleToggle = (e) => {
-    setIsOpen(!iseOpen);
-    e.stopPropagation();
-    props.onToggle(props.node.id);
-  };
+    setIsOpen(!iseOpen)
+    e.stopPropagation()
+    props.onToggle(props.node.id)
+  }
   const handleShowInput = () => {
-    setVisibleInput(true);
-  };
+    setVisibleInput(true)
+  }
   const handleCancel = () => {
-    setLabelText(text);
-    setVisibleInput(false);
-  };
+    setLabelText(text)
+    setVisibleInput(false)
+  }
 
   const handleChangeText = (e) => {
-    setLabelText(e.target.value);
-  };
+    setLabelText(e.target.value)
+  }
 
   const handleSubmit = () => {
-    setVisibleInput(false);
-    props.onTextChange(id, labelText);
-  };
-  const dragOverProps = useDragOver(id, props.isOpen, props.onToggle);
-  const [inputValue, setInputValue] = useState({ id: null, value: 7 });
+    setVisibleInput(false)
+    props.onTextChange(id, labelText)
+  }
+  const dragOverProps = useDragOver(id, props.isOpen, props.onToggle)
+  const [inputValue, setInputValue] = useState({ id: null, value: 7 })
   const onChange = (value) => {
-    setInputValue({ id: props.node.id, value });
+    setInputValue({ id: props.node.id, value })
     // eslint-disable-next-line no-unused-expressions
-    props.onChangeTime({ id: props.node.id, value });
-  };
+    props.onChangeTime({ id: props.node.id, value })
+  }
   function onAfterChange(value) {
-    props.onAfterChange({ id: props.node.id, value });
+    props.onAfterChange({ id: props.node.id, value })
   }
   return (
     <div
-      className={`tree-node mb-1 ${styles.root}`}
-      style={{ paddingInlineStart: indent }}
+      className={`tree-node ${styles.root}`}
       {...dragOverProps}
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
@@ -82,7 +82,7 @@ export const CustomNode = (props) => {
             <a
               className={styles.editButton}
               onClick={handleSubmit}
-              disabled={labelText === ""}
+              disabled={labelText === ''}
             >
               <CheckOutlined className="mx-1" />
             </a>
@@ -97,42 +97,52 @@ export const CustomNode = (props) => {
                 <Row gutter={[50, 50]}>
                   <Col span={12}>
                     <Row gutter={[50, 50]}>
-                      <Col span={12}>
-                        <div>
-                          {props.node.droppable && (
-                            <div onClick={handleToggle}>
-                              {iseOpen ? <DownOutlined /> : <RightOutlined />}
-                            </div>
-                          )}
+                      <Col span={20}>
+                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                          <div>
+                            {props.node.droppable && (
+                              <div onClick={handleToggle}>
+                                {iseOpen ? <DownOutlined className="mr-1" /> : <RightOutlined className="mr-1" />}
+                              </div>
+                            )}
+                          </div>
+                          <div className="mr-1">
+                            {props.node.droppable ? (
+                              <FolderFilled />
+                            ) : (
+                              props.node.parent ? <FileFilled className="ml-4" />
+                                : <FileFilled />
+                            )}
+                          </div>
+                          <div className="text">
+                            <span>{props.node.text}</span>
+                            {' '}
+                          </div>
                         </div>
-                        <div className="mr-1">
-                          {props.node.droppable ? (
-                            <FolderFilled />
-                          ) : (
-                            <FileFilled />
-                          )}
-                        </div>
-                        <div className="text">{props.node.text}</div>
+
                       </Col>
-                      <Col span={12}>
-                        <div className={styles.actionButton}>
-                          <a className="mr-1" onClick={handleShowInput}>
-                            <EditTwoTone className="" />
-                          </a>
-                          <a
-                            size="small ml-10"
-                            onClick={() => props.onDelete(id)}
-                          >
-                            <DeleteTwoTone />
-                          </a>
-                        </div>
+                      <Col span={4}>
+                        {props.node.droppable ? (
+                          <div className={styles.actionButton}>
+                            <a className="mr-1" onClick={handleShowInput}>
+                              <EditTwoTone className="" />
+                            </a>
+                            <a
+                              size="small ml-10"
+                              onClick={() => props.onDelete(id)}
+                            >
+                              <DeleteTwoTone />
+                            </a>
+                          </div>
+                        ) : null}
+
                       </Col>
                     </Row>
 
-                    <div className="flex items-center"></div>
+                    <div className="flex items-center" />
                   </Col>
                   <Col span={12}>
-                    {" "}
+                    {' '}
                     <div className="item-task">
                       {!props.node.droppable ? (
                         <div className="">
@@ -143,7 +153,7 @@ export const CustomNode = (props) => {
                             onAfterChange={onAfterChange}
                             defaultValue={7}
                             value={
-                              typeof inputValue.value === "number"
+                              typeof inputValue.value === 'number'
                                 ? inputValue.value
                                 : 0
                             }
@@ -160,5 +170,5 @@ export const CustomNode = (props) => {
         )}
       </div>
     </div>
-  );
-};
+  )
+}
